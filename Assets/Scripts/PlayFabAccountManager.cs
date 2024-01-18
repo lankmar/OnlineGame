bb891e5b1f3da3b711d68fe48f07fe088b02564f
lankmar <32324313+lankmar@users.noreply.github.com>
2024-01-18 15:33:27 +0300

    05
    home work 5

--- Assets/Scripts/PlayFabAccountManager.cs
+++ Assets/Scripts/PlayFabAccountManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using PlayFab;
 using PlayFab.ClientModels;
 using System;
+using System.Collections.Generic;
 
 public class PlayFabAccountManager : MonoBehaviour
 {
@@ -12,12 +13,39 @@ public class PlayFabAccountManager : MonoBehaviour
     void Start()
     {
         PlayFabClientAPI.GetAccountInfo(new GetAccountInfoRequest(), OnGetAccount, OnError);    
+        PlayFabClientAPI.GetCatalogItems(new GetCatalogItemsRequest(), OnGetCatalogSuccess, OnError);
+        PlayFabServerAPI.GetRandomResultTables(new PlayFab.ServerModels.GetRandomResultTablesRequest(), OnGetRandomResult, OnError);
+    }
+
+    private void OnGetRandomResult(PlayFab.ServerModels.GetRandomResultTablesResult result)
+    {
+        if (result != null)
+        {
+            var count = result.Tables.Keys;
+
+        }
     }
 
     private void OnGetAccount(GetAccountInfoResult result)
     {
         _textLabel.text = $"Player id: {result.AccountInfo.PlayFabId}";
         _dopInformation.text = $"Username: {result.AccountInfo.Username},  Created: {result.AccountInfo.Created}";
+    }
+
+    private void OnGetCatalogSuccess(GetCatalogItemsResult result)
+    {
+        Debug.Log("OnGetCatalogSuccess");
+        ShoeItems(result.Catalog);
+        //_textLabel.text = $"Player id: {result.AccountInfo.PlayFabId}";
+        //_dopInformation.text = $"Username: {result.AccountInfo.Username},  Created: {result.AccountInfo.Created}";
+    }
+
+    private void ShoeItems(List<CatalogItem> catalog)
+    {
+        foreach (var item in catalog)
+        {
+            Debug.Log($"{item.DisplayName}");
+        }
     }
 
     private void OnError(PlayFabError error)
